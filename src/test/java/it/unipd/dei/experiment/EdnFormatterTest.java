693603523dadee301efd7dcb12aad19a77d317a3
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2014-06-13 15:43:40 +0200

    Add EdnFormatterTest

---
+++ src/test/java/it/unipd/dei/experiment/EdnFormatterTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2014 Matteo Ceccarello
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package it.unipd.dei.experiment;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import clojure.lang.*;
+import org.junit.Test;
+
+public class EdnFormatterTest {
+
+  public static final Keyword CLASS = Keyword.intern(null, "class");
+  public static final Keyword NAME = Keyword.intern(null, "name");
+  public static final Keyword DATE = Keyword.intern(null, "date");
+  public static final Keyword NOTES = Keyword.intern(null, "notes");
+  public static final Keyword TAGS = Keyword.intern(null, "tags");
+  public static final Keyword TABLES = Keyword.intern(null, "tables");
+
+  @Test
+  public void testEdn1() {
+    Experiment exp = new Experiment("exp-class", "exp-name");
+    String edn = EdnFormatter.format(exp);
+    Object parsed = RT.readString(edn);
+
+    assertThat(parsed, is(instanceOf(IPersistentMap.class)));
+
+    IPersistentMap expMap = (IPersistentMap) parsed;
+
+    assertEquals(expMap.valAt(CLASS), "exp-class");
+    assertEquals(expMap.valAt(NAME), "exp-name");
+    assertEquals(expMap.valAt(NOTES), PersistentVector.EMPTY);
+    assertEquals(expMap.valAt(TAGS), PersistentArrayMap.EMPTY);
+    assertEquals(expMap.valAt(TABLES), PersistentArrayMap.EMPTY);
+  }
+
+}
